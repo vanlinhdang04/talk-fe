d75513e28b125825506887b22cad1c9486247f61
vanlinhdang04 <vanlinhdang1999@gmail.com>
2023-07-14 15:05:20 +0700

    update config vite

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-router", "react-router-dom"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
-  },
+  base: './',
+  // build: {
+  //   rollupOptions: {
+  //     external: ["react", "react-router", "react-router-dom"],
+  //     output: {
+  //       globals: {
+  //         react: "React",
+  //       },
+  //     },
+  //   },
+  // },
 });
